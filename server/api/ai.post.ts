4dbddf11d15b433e61399ecf711b737c846227a5
lee-vincent <vinnie@vinnielee.io>
2023-09-11 06:22:29 -0400

    create ai.posts endpoint

---
+++ server/api/ai.post.ts
@@ -0,0 +1,20 @@
+import { Configuration, OpenAIApi } from "openai";
+
+export default defineEventHandler(async (event) => {
+  
+  const { OPENAI_API_KEY } = useRuntimeConfig();
+
+  const configuration = new Configuration({
+    apiKey: OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "what is a 5 digit prime number?" }],
+  });
+
+  return completion.data;
+
+});
