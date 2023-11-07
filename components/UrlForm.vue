8f2ac08e4c78ddfd75b505a3dcde84adf83b9bbc
lee-vincent <vinnie@vinnielee.io>
2023-11-07 10:04:19 -0500

    basic integration with partner microservice

--- components/UrlForm.vue
+++ components/UrlForm.vue
@@ -1,19 +1,34 @@
 <script setup lang="ts">
+	import { ref, onMounted } from "vue";
+	import axios from "axios";
+
 	const props = defineProps<{
 		url: string;
 		temperature: number;
 	}>();
+
+	const placeholder = ref({});
+
 	defineEmits(["submit"]);
+
 	const form = reactive({
 		url: props.url,
 		temperature: props.temperature,
 	});
+
+
+	onMounted(async () => {
+		const response = await axios.get(`http://54.224.83.122:3000/api/rand`);
+		console.log(response.data);
+		placeholder.value = response.data;
+	});
+
 </script>
 <template>
 	<form @submit.prevent="$emit('submit', form)" class="mb-10">
 		<div class="flex">
 			<input
-				placeholder="What should we 'Dad Joke' about? Cats? Dogs? Anything!"
+				placeholder="What should we dad joke about? Cats? Dogs? Anything!"
 				v-model="form.url"
 				class="input input-bordered rounded-r-none w-full max-w-full"
 				type="text" />
